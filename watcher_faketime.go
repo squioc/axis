1fadc1d8f5ea6d229b75b7852f06068566a15ea1
Quioc Sébastien <sebastien.quioc@yahoo.com>
2015-05-24 12:13:28 +0200

    Add the watcher for the fake time provider

---
+++ watcher_faketime.go
@@ -0,0 +1,20 @@
+package axis
+
+
+type FakeTimeWatcher struct {
+    can_reset bool
+    can_stop bool
+}
+
+func newFakeTimeWatcher(can_reset, can_stop bool) *FakeTimeWatcher {
+    return &FakeTimeWatcher{can_reset: can_reset, can_stop: can_stop}
+}
+
+func (s *FakeTimeWatcher) Reset(distance Distance) bool {
+    return s.can_reset
+}
+
+func (s *FakeTimeWatcher) Stop() bool {
+    return s.can_stop
+}
+
---
+++ watcher_faketime_test.go
@@ -0,0 +1,39 @@
+package axis
+
+import (
+    "testing"
+)
+
+
+func TestFakeTimeWatcherReset(t *testing.T) {
+    // Arrange
+    can_reset := true
+    can_stop := true
+    distance := Distance(0)
+    watcher := newFakeTimeWatcher(can_reset, can_stop)
+    var actual bool
+
+    // Act
+    actual = watcher.Reset(distance)
+
+    // Assert
+    if actual != can_reset  {
+        t.Fatalf("Mismatching expected boolean for the reset of the fake time watcher")
+    }
+}
+
+func TestFaketimeWatcherStop(t *testing.T) {
+    // Arrange
+    can_reset := true
+    can_stop := true
+    watcher := newFakeTimeWatcher(can_reset, can_stop)
+    var actual bool
+
+    // Act
+    actual = watcher.Stop()
+
+    // Assert
+    if actual != can_stop  {
+        t.Fatalf("Mismatching expected boolean for the stop of the fake time watcher")
+    }
+}
