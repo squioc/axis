23e9f8fae329f0527ed22317abb85a5859546417
Quioc Sébastien <sebastien.quioc@yahoo.com>
2016-02-18 19:33:51 +0100

    Split the Provider interface into narrow ones

--- provider.go
+++ provider.go
@@ -1,14 +1,31 @@
 package axis
 
-// Provider is the interface that wraps methods
-// to manipulate position
-type Provider interface {
+// Positionable is the interface for positionable
+// items on a axis
+type Positionable interface {
 	Current() Position
+	Since(Position) Distance
+}
+
+// Sleepable is the interface for sleepable provider
+type Sleepable interface {
 	Sleep(Distance)
+}
+
+// Trigger is the interface that wraps methods
+// to define triggers
+type Trigger interface {
 	After(Distance) <-chan Position
 	AfterFunc(Distance, func(Position)) Watcher
 	AfterChan(Distance, chan Position) Watcher
-	Since(Position) Distance
+}
+
+// Provider is the interface that wraps methods
+// to manipulate position
+type Provider interface {
+        Positionable
+        Sleepable
+        Trigger
 }
 
 // UpdatableProvider is the interface which allow
